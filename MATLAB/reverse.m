bf79ce79f0946f119f8b67efd1b5b8c8ebd9b8ef
rohans0509 <59282751+rohans0509@users.noreply.github.com>
2020-11-06 16:44:26 +0530

    Add networks live script

---
+++ MATLAB/reverse.m
@@ -0,0 +1,10 @@
+function out= reverse(list)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+list=1./list;
+out=cell(1,length(list));
+for k = 1:length(list)
+        out{k} = list(k);
+end 
+end
+
--- MATLAB/solvediff.m
+++ MATLAB/solvediff.m
@@ -4,13 +4,11 @@ function varargout = solvediff(Y,A,B,Y0)
 syms sol(t);
 odes = diff(Y) == A*Y + B;
 initialcond=Y(0)==Y0;
-display(odes);
 if exist('Y0','var')
     Y = dsolve(odes,initialcond);
 else
     Y = dsolve(odes);
 end
-display(Y)
 varargout=cell(nargout);
 if nargout==1
     varargout{1}=Y;
@@ -18,7 +16,6 @@ else
     fns=fieldnames(Y);
     for k = 1:nargout
         sol=Y.(fns{k});
-        display(sol)
         varargout{k} = simplify(sol);
     end
 end
